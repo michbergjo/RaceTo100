e876f89d6ba9aebf261c47070fcea5f5a29c4546
Michelle Joseph <mbergy27@gmail.com>
2021-08-06 16:34:58 -0500

    Added JavaScript for app functionality

--- js/site.js
+++ js/site.js
@@ -1,4 +1,59 @@
-// Call Hello World 
-function HelloWorld(){
-    alert("hello world");
+
+
+//get the values from the page
+//program starts here, controller function 
+function getValues(){
+    //get values from the page 
+    let startValue = document.getElementById("startValue").value;
+    let endValue = document.getElementById("endValue").value;
+
+    //parse into Integers
+    startValue = parseInt(startValue);
+    endValue = parseInt(endValue);
+
+    //check that inputs are integers
+    if (Number.isInteger(startValue) && Number.isInteger(endValue)) {
+        //call generateNumbers
+        let numbers = generateNumbers(startValue, endValue);
+         //call displayNumbers
+        displayNumbers(numbers);
+    } else {
+        alert("You must enter integers");
+    }
+
+}
+
+//generate numbers from start value to the end value 
+//logic function
+function generateNumbers(startValue, endValue){
+
+    let numbers = [];
+
+    //get all numbers from start to end 
+    for(let i = startValue; i <= endValue; i++){
+        numbers.push(i);
+    }
+
+    return numbers;
+}
+
+//display the numbers, mark even numbers in bold 
+//display or view function
+function displayNumbers(numbers){
+    let templateRows = "";
+
+    for (let index = 0; index < numbers.length; index++) {
+        let className = "even";
+        let number = numbers[index];
+        if (number % 2 == 0) {
+            className = "even";
+        } else {
+            className = "odd";
+        }
+
+        templateRows += `<tr><td class="${className}">${number}</td><tr>`;
+    }
+
+    document.getElementById("results").innerHTML = templateRows;
+
 }
